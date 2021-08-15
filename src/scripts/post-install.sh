ee8ff552869d7e35b3f4e111f5d3d8106c6263b2
Andri Sudarmawijaya <andri.sudarmawijaya@gmail.com>
2021-08-15 10:25:47 +0700

    v0.0.1

---
+++ src/scripts/post-install.sh
@@ -0,0 +1,11 @@
+mkdir ../../../application/views/_layouts
+cp src/application/views/_layouts/index.html ../../../application/views/_layouts/index.html
+cp src/application/views/_layouts/layout.php ../../../application/views/_layouts/_layout.php
+cp src/application/views/_layouts/index.php ../../../application/views/_layouts/index.php
+cp src/application/views/_layouts/head.php ../../../application/views/_layouts/head.php
+cp src/application/views/_layouts/header.php ../../../application/views/_layouts/header.php
+cp src/application/views/_layouts/navbar.php ../../../application/views/_layouts/navbar.php
+cp src/application/views/_layouts/sidebar.php ../../../application/views/_layouts/sidebar.php
+cp src/application/views/_layouts/aside.php ../../../application/views/_layouts/aside.php
+cp src/application/views/_layouts/footer.php ../../../application/views/_layouts/footer.php
+cp src/application/views/_layouts/script.php ../../../application/views/_layouts/script.php
---
+++ src/scripts/post-update.sh
@@ -0,0 +1,6 @@
+cp src/application/controllers/Customer.php ../../../application/controllers/Customer.php
+cp src/application/models/Customer_model.php ../../../application/models/Customer_model.php
+cp src/application/views/customer/index.html ../../../application/views/customer/index.html
+cp src/application/views/customer/customer_list.php ../../../application/views/customer/customer_list.php
+cp src/application/views/customer/customer_form.php ../../../application/views/customer/customer_form.php
+cp src/application/views/customer/customer_read.php ../../../application/views/customer/customer_read.php
