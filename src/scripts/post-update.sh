380ae8fdd9cc9fe2685c7ddf445945c28429dbf0
Andri Sudarmawijaya <andri.sudarmawijaya@gmail.com>
2021-08-15 11:04:49 +0700

    v0.0.1

--- src/scripts/post-update.sh
+++ src/scripts/post-update.sh
@@ -1,6 +1,5 @@
 mkdir ../../../application/views/_layouts
 cp src/application/views/_layouts/index.html ../../../application/views/_layouts/index.html
-cp src/application/views/_layouts/layout.php ../../../application/views/_layouts/_layout.php
 cp src/application/views/_layouts/index.php ../../../application/views/_layouts/index.php
 cp src/application/views/_layouts/head.php ../../../application/views/_layouts/head.php
 cp src/application/views/_layouts/header.php ../../../application/views/_layouts/header.php
